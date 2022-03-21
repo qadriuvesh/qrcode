48c9a3e99223807f808439ae474fe65a37a74f93
qadriuvesh <qadriuvesh76@gmail.com>
2022-03-21 12:58:05 +0530

    hellow world

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ generateQr.addEventListener('click',()=>{
         generateQr.innerText = 'Generate QR Code'        
     })
 })
+
+window.addEventListener('keydown',(e)=>{
+    if(e.key =='Enter'){
+    let qrInput = document.querySelector('#qrInput').value;
+    generateQr.innerText = 'Generating QR Code...'
+    imgContainer.style.height="0px";
+    qrImage.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrInput}`;
+    qrImage.addEventListener('load',()=>{
+        imgContainer.style.height="150px";
+        generateQr.innerText = 'Generate QR Code'        
+    })
+    }
+})
